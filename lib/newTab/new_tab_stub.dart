63b81356b627eb9b2ba47a1a7c10fb68b56407ba
richardnovasnw <88481980+richardnovasnw@users.noreply.github.com>
2021-11-05 20:58:07 +0530

    Delete lib directory

--- lib/newTab/new_tab_stub.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-void tabMethod(BuildContext context) {
-  ScaffoldMessenger.of(context)
-      .showSnackBar(const SnackBar(content: Text('Unsupported')));
-}
--- lib/newTab/web.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:html' as html;
-
-import 'package:flutter/widgets.dart';
-
-void tabMethod(BuildContext context) {
-  html.window.open('https://flutter.dev/', "new tab");
-}
